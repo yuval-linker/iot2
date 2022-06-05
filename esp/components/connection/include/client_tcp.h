0c4067d083331633899121d5934d776576266138
yuval-linker <yuvallinkerg@gmail.com>
2022-06-04 22:00:26 -0400

    receive messages with tcp

--- esp/components/connection/include/client_tcp.h
+++ esp/components/connection/include/client_tcp.h
@@ -5,3 +5,4 @@
 int tcp_connect(int host_ip, int port);
 void tcp_send(int sock, unsigned char* payload, int payload_size, char protocol_id);
 void tcp_socket_close(int sock);
+int tcp_recv(int sock, char* buffer, int buffer_size);
--- esp/components/connection/src/client_tcp.c
+++ esp/components/connection/src/client_tcp.c
@@ -3,9 +3,10 @@
 #include "esp_netif.h"
 #include "lwip/err.h"
 #include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 
 #include "send_payload.h"
-
 #include "client_tcp.h"
 
 static const char *TCP_TAG = "ESP32 TCP";
@@ -45,6 +46,22 @@ void tcp_send(int sock, unsigned char* payload, int payload_size, char protocol_
       return;
   }
   ESP_LOGI(TCP_TAG, "Message sent");
+}
+
+int tcp_recv(int sock, char* buffer, int buffer_size) {
+  int len = recv(sock, buffer, sizeof(buffer_size) - 1, 0);
+  // Error occurred during receiving
+  if (len < 0) {
+      ESP_LOGE(TCP_TAG, "recv failed: errno %d", errno);
+      return -1;
+  }
+  // Data received
+  else {
+      buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+      ESP_LOGI(TCP_TAG, "Received %d bytes", len);
+      ESP_LOGI(TCP_TAG, "Message: %s", buffer);
+  }
+  return len;
 }
 
 void tcp_socket_close(int sock) {
